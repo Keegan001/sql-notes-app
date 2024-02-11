742c3e24d4002c9b38b15f37236340c66f882192
Divyam Jain <jain.divyam.271@gmail.com>
2024-02-11 18:44:31 +0530

    added funtionality to sql

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_tutorial_1/db_handler.dart';
+import 'package:flutter_tutorial_1/notes.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -8,6 +10,15 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  DBHelper? dbHelper;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    dbHelper = DBHelper();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,6 +30,28 @@ class _HomeScreenState extends State<HomeScreen> {
       ),
       body: Column(
         children: [],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          dbHelper!
+              .insert(
+            NotesModel(
+                title: 'first note',
+                age: 19,
+                description: 'first sql app',
+                email: 'xyz@gmail.com'),
+          )
+              .then(
+            (value) {
+              print('data added');
+            },
+          ).onError(
+            (error, stackTrace) {
+              print(error.toString());
+            },
+          );
+        },
+        child: Icon(Icons.add),
       ),
     );
   }
